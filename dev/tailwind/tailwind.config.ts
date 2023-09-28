ace7b63d007d7584a5f1d2d17224d7c7d2d49f6b
johancs.mm@gmail.com <johancs.mm@gmail.com>
2023-09-28 17:11:57 -0500

    chore: Add ui pkg

--- dev/tailwind/tailwind.config.ts
+++ dev/tailwind/tailwind.config.ts
@@ -1,14 +1,9 @@
 import type { Config } from "tailwindcss";
 
 const config: Config = {
-  content: ["./src/**/*.{js,ts,jsx,tsx,mdx}"],
+  content: ["./app/**/*.{js,ts,jsx,tsx,mdx}", "./components/**/*.{js,ts,jsx,tsx,mdx}"],
   theme: {
-    extend: {
-      backgroundImage: {
-        "glow-conic":
-          "conic-gradient(from 180deg at 50% 50%, #2a8af6 0deg, #a853ba 180deg, #e92a67 360deg)",
-      },
-    },
+    extend: {},
   },
   plugins: [],
 };
