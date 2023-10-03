dded838ee2ef6a02869c67823e8d3a20531bcfa7
johancs.mm@gmail.com <johancs.mm@gmail.com>
2023-10-03 10:22:36 -0500

    feat: succesfuly code approved

--- dev/ui/components/button.tsx
+++ dev/ui/components/button.tsx
@@ -1,12 +1,11 @@
 import { ButtonProps } from './interfaces/dev';
-import { STYLED_CSS } from "./interfaces/const";
+import { STYLED_CSS } from './interfaces/const';
 
-export function Button({ children, shadow, rounded,...atributtes }: ButtonProps): JSX.Element {
-
+export function Button({ children, shadow, rounded, ...atributtes }: ButtonProps): JSX.Element {
   const styled = {
     boxShadow: shadow ? STYLED_CSS.shadow : 'none',
-    borderRadius: rounded ? "1rem" : "none",
-  }
+    borderRadius: rounded ? '1rem' : 'none',
+  };
 
   return (
     <button type='button' {...atributtes} style={styled}>
--- packages/labs/app/page.tsx
+++ packages/labs/app/page.tsx
@@ -4,38 +4,38 @@ import Header from './header';
 export default function HomePage() {
   return (
     <>
-    <div className='bg-[#050407]'>
-      <Header />
-      <div className='relative isolate px-6 pt-14 lg:px-8'>
-        <div className='py-32 sm:py-48 lg:py-48'>
-          <div className='text-center max-w-7xl mx-auto'>
-            <h2 className='text-6xl flex justify-center items-center flex-wrap font-bold tracking-tight text-white sm:text-8xl font-inter w-full px-2'>
-              <span className='block relative p-2'>Laburar.</span>
-              <span className='block relative p-2'>Avanzar.</span>
-              <span className='block relative p-2'>Brillar.</span>
-            </h2>
-            <p className='mt-6 text-xl text-[#888] p-0 lg:px-72'>
-              Labs es una empresa líder en el desarrollo de páginas web que se dedica a transformar
-              tus ideas en experiencias digitales excepcionales.
-            </p>
-            <div className='mt-14 flex items-center justify-center gap-x-8'>
-              <a
-                href='#'
-                className='rounded-md bg-indigo-600 px-6 py-2.5 text-white shadow-sm hover:bg-indigo-500 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600 font-inter w-40 text-lg'
-              >
-                Servicios
-              </a>
-              <a href='#' className='text-lg leading-6 text-white font-inter'>
-                Contactanos
-              </a>
+      <div className='bg-[#050407]'>
+        <Header />
+        <div className='relative isolate px-6 pt-14 lg:px-8'>
+          <div className='py-32 sm:py-48 lg:py-48'>
+            <div className='text-center max-w-7xl mx-auto'>
+              <h2 className='text-6xl flex justify-center items-center flex-wrap font-bold tracking-tight text-white sm:text-8xl font-inter w-full px-2'>
+                <span className='block relative p-2'>Laburar.</span>
+                <span className='block relative p-2'>Avanzar.</span>
+                <span className='block relative p-2'>Brillar.</span>
+              </h2>
+              <p className='mt-6 text-xl text-[#888] p-0 lg:px-72'>
+                Labs es una empresa líder en el desarrollo de páginas web que se dedica a
+                transformar tus ideas en experiencias digitales excepcionales.
+              </p>
+              <div className='mt-14 flex items-center justify-center gap-x-8'>
+                <a
+                  href='#'
+                  className='rounded-md bg-indigo-600 px-6 py-2.5 text-white shadow-sm hover:bg-indigo-500 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600 font-inter w-40 text-lg'
+                >
+                  Servicios
+                </a>
+                <a href='#' className='text-lg leading-6 text-white font-inter'>
+                  Contactanos
+                </a>
+              </div>
             </div>
           </div>
         </div>
       </div>
-    </div>
-    <Button className='text-white bg-purple-500 p-4' shadow>
-          lABS COMPONENTE
-    </Button>
+      <Button className='text-white bg-purple-500 p-4' shadow>
+        lABS COMPONENTE
+      </Button>
     </>
   );
 }
